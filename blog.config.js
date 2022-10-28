402bc81f5046d942b47d42c57df87e1d4ee19327
chifflyne <116882892+chifflyne@users.noreply.github.com>
2022-10-28 18:59:48 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -2,8 +2,8 @@
 const BLOG = {
   AUTHOR: 'Chyfflyne', // 作者
   BIO: '各類筆記/素材放置區', // 作者简介
-  LINK: 'https://tangly1024.com', // 网站地址
-  KEYWORDS: 'Notion, 博客', // 网站关键词 英文逗号隔开
+  LINK: 'https://chifflyne-notion.vercel.app/', // 网站地址
+  KEYWORDS: 'Notion, ', // 网站关键词 英文逗号隔开
   NOTION_PAGE_ID:
   process.env.NOTION_PAGE_ID || '02ab3b8678004aa69e9e415905ef32a5', // Important page_id！！！Duplicate Template from  https://www.notion.so/tanghh/02ab3b8678004aa69e9e415905ef32a5
   NOTION_ACCESS_TOKEN: process.env.NOTION_ACCESS_TOKEN || '', // Useful if you prefer not to make your database public
