1d8c634ae30cb5676113d38883da5022ead57632
chifflyne <116882892+chifflyne@users.noreply.github.com>
2022-10-28 20:20:50 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -32,7 +32,7 @@ const BLOG = {
   CUSTOM_FONT_SERIF: ['FakePearl-Regular'], // 自定义衬线字体
 
   // 图标字体
-  FONT_AWESOME_PATH: 'https://cdn.bootcdn.net/ajax/libs/font-awesome/5.15.4/css/all.min.css', // 图标库CDN ，国内推荐BootCDN，国外推荐 CloudFlare https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css
+  FONT_AWESOME_PATH: 'https://cdn.jsdelivr.net/gh/max32002/FakePearl@1.1/webfont/FakePearl-Regular.woff2', // 图标库CDN ，国内推荐BootCDN，国外推荐 CloudFlare https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css
 
   BACKGROUND_LIGHT: '#eeeeee', // use hex value, don't forget '#' e.g #fffefc
   BACKGROUND_DARK: '#000000', // use hex value, don't forget '#'
