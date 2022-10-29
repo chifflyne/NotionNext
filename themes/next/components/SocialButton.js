6a56076af47b3b434c32d057273034779c8590c1
chifflyne <116882892+chifflyne@users.noreply.github.com>
2022-10-29 17:06:00 +0800

    Update SocialButton.js

--- themes/next/components/SocialButton.js
+++ themes/next/components/SocialButton.js
@@ -30,7 +30,7 @@ const SocialButton = () => {
       {BLOG.CONTACT_EMAIL && <a target='_blank' rel='noreferrer' title={'email'} href={`mailto:${BLOG.CONTACT_EMAIL}`} >
         <i className='fas fa-envelope transform hover:scale-125 duration-150'/>
       </a>}
-      {BLOG.CONTACT_TWITCH && <a target='_blank' rel='noreferrer' title={'twitch'} href={`mailto:${BLOG.CONTACT_TWITCH}`} >
+      {BLOG.CONTACT_TWITCH && <a target='_blank' rel='noreferrer' title={'twitch'} href={BLOG.CONTACT_TWITCH} >
         <i className='fas fa-twitch transform hover:scale-125 duration-150'/>
       </a>}
       <a target='_blank' rel='noreferrer' title={'RSS'} href={'/feed'} >
