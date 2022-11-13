0a1fd8568440ffac85661e0a596ae33c14400afc
chifflyne <116882892+chifflyne@users.noreply.github.com>
2022-11-13 23:51:44 +0800

    Update SocialButton.js

--- themes/next/components/SocialButton.js
+++ themes/next/components/SocialButton.js
@@ -1,15 +1,3 @@
-import BLOG from '@/blog.config'
-import React from 'react'
-
-/**
- * 社交联系方式按钮组
- * @returns {JSX.Element}
- * @constructor
- */
-const SocialButton = () => {
-  return <div className='w-full justify-center flex-wrap flex'>
-    <div className='space-x-3 text-xl text-gray-600 dark:text-gray-400 '>
-      {BLOG.CONTACT_YOUTUBE && <a target="_blank" rel="noreferrer" title={'youtube'} href={BLOG.CONTACT_YOUTUBE} >
         <i className="fab fa-youtube transform hover:scale-125 duration-150  text-red-400" />
       </a>}
       {BLOG.CONTACT_GITHUB && <a target='_blank' rel='noreferrer' title={'github'} href={BLOG.CONTACT_GITHUB} >
@@ -18,8 +6,17 @@ const SocialButton = () => {
       {BLOG.CONTACT_TWITTER && <a target='_blank' rel='noreferrer' title={'twitter'} href={BLOG.CONTACT_TWITTER} >
         <i className='fab fa-twitter transform hover:scale-125 duration-150 text-blue-400'/>
       </a>}
-      {BLOG.CONTACT_TWITCH && <a target='_blank' rel='noreferrer' title={'twitter'} href={BLOG.CONTACT_TWITTER} >
-        <i className='fab fa-twitch transform hover:scale-125 duration-150 text-indigo-400'/>
+      {BLOG.CONTACT_DISCORD && <a target='_blank' rel='noreferrer' title={'discord'} href={BLOG.CONTACT_DISCORD} >
+        <i className='fab fa-discord transform hover:scale-125 duration-150 text-indigo-400'/>
+      </a>}
+      {BLOG.CONTACT_TELEGRAM && <a target='_blank' rel='noreferrer' href={BLOG.CONTACT_TELEGRAM} title={'telegram'} >
+        <i className="fab fa-youtube transform hover:scale-125 duration-150  text-red-400" />
+      </a>}
+      {BLOG.CONTACT_GITHUB && <a target='_blank' rel='noreferrer' title={'github'} href={BLOG.CONTACT_GITHUB} >
+        <i className='fab fa-github transform hover:scale-125 duration-150'/>
+      </a>}
+      {BLOG.CONTACT_TWITTER && <a target='_blank' rel='noreferrer' title={'twitter'} href={BLOG.CONTACT_TWITTER} >
+        <i className='fab fa-twitter transform hover:scale-125 duration-150 text-blue-400'/>
       </a>}
       {BLOG.CONTACT_DISCORD && <a target='_blank' rel='noreferrer' title={'discord'} href={BLOG.CONTACT_DISCORD} >
         <i className='fab fa-discord transform hover:scale-125 duration-150 text-indigo-400'/>
@@ -32,6 +29,9 @@ const SocialButton = () => {
       </a>}
       {BLOG.CONTACT_EMAIL && <a target='_blank' rel='noreferrer' title={'email'} href={`mailto:${BLOG.CONTACT_EMAIL}`} >
         <i className='fas fa-envelope transform hover:scale-125 duration-150'/>
+      </a>}
+      {BLOG.CONTACT_TWITCH && <a target='_blank' rel='noreferrer' title={'twitch'} href={BLOG.CONTACT_TWITCH} >
+        <i className='fab fa-twitch transform hover:scale-125 duration-150 text-indigo-400'/>
       </a>}
       <a target='_blank' rel='noreferrer' title={'RSS'} href={'/feed'} >
         <i className='fas fa-rss transform hover:scale-125 duration-150 text-orange-500'/>
