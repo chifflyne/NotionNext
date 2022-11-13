b1c2928f8e16c9a924c002abd266ad3b45602668
chifflyne <116882892+chifflyne@users.noreply.github.com>
2022-11-14 01:41:08 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,7 +1,7 @@
 // 注: process.env.XX是Vercel的环境变量，配置方式见：https://docs.tangly1024.com/zh/features/personality
 const BLOG = {
   AUTHOR: 'Chifflyne', // 作者
-  BIO: '惡魔吉娃', // 作者简介
+  BIO: '惡魔吉娃，最愛拌咖哩飯', // 作者简介
   LINK: 'https://chifflyne-notion.vercel.app', // 网站地址
   KEYWORDS: 'mmd, vroid, 3d', // 网站关键词 英文逗号隔开
   NOTION_PAGE_ID:
