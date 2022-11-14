59c428c115338da97ce3fd683ae14b060cb70974
chifflyne <116882892+chifflyne@users.noreply.github.com>
2022-11-14 20:44:13 +0800

    Update SocialButton.js

--- themes/next/components/SocialButton.js
+++ themes/next/components/SocialButton.js
@@ -30,6 +30,9 @@ import React from 'react'
        {BLOG.CONTACT_EMAIL && <a target='_blank' rel='noreferrer' title={'email'} href={`mailto:${BLOG.CONTACT_EMAIL}`} >
          <i className='fas fa-envelope transform hover:scale-125 duration-150'/>
        </a>}
+      {BLOG.CONTACT_TWITCH && <a target='_blank' rel='noreferrer' title={'twitch'} href={BLOG.CONTACT_TWITCH} >
+         <i className='fab fa-twitch transform hover:scale-125 duration-150 text-indigo-400'/>
+       </a>}
        <a target='_blank' rel='noreferrer' title={'RSS'} href={'/feed'} >
          <i className='fas fa-rss transform hover:scale-125 duration-150 text-orange-500'/>
        </a>
